69d59a314347b50be1da46576fe5e63008a1c108
Anna Wu <121405231+annacwu@users.noreply.github.com>
2023-09-22 21:18:52 -0700

    Delete homework1 directory

--- homework1/distance.py
+++
@@ -1,7 +0,0 @@
-from math import sqrt
-x1 = input("Location x-coordinate? ")
-y1 = input("Location y-coordinate? ")
-x2 = input("Classroom x-coordinate? ")
-y2 = input("Classroom y-coordinate? ")
-print("Distance:")
-print(sqrt(((float(x2) - float(x1)) ** 2) + ((float(y2) - float(y1)) ** 2)))
