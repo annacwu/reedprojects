2bee7844874274de6ed53d0dc1f37cc35bd0a807
Anna Wu <121405231+annacwu@users.noreply.github.com>
2023-09-22 21:19:31 -0700

    Delete homework3 directory

--- homework3/th.py
+++
@@ -1,9 +0,0 @@
-def th(rank):
-    if(rank == 1):
-        return "1st"
-    elif(rank == 2):
-        return "2nd"
-    elif(rank == 3):
-        return "3rd"
-    else:
-        return str(rank) + "th"
