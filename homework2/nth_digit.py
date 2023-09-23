1373a02613717f31e4ab4b68839dbe7951202f82
Anna Wu <121405231+annacwu@users.noreply.github.com>
2023-09-22 21:19:18 -0700

    Delete homework2 directory

--- homework2/nth_digit.py
+++
@@ -1,4 +0,0 @@
-main = input("Enter a non-negative integer: ")
-pos = input("Enter a digit position: ")
-output = int(main) // 10 ** int(pos) % 10
-print("The " + str(10**int(pos)) + "s digit of that integer is " + str(output) + ".")
--- homework2/reverse.py
+++
@@ -1,5 +0,0 @@
-integer = input("Enter a 3-digit integer: ")
-firstdigit = int(integer) % 10
-seconddigit = int(integer) // 10 % 10
-thirddigit = int(integer) // 10 // 10 % 10
-print(str(firstdigit)+ str(seconddigit) + str(thirddigit))
