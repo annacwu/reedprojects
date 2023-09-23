5c3775071f8845dc5a20b7c30a0cb554e72e3547
Anna Wu <121405231+annacwu@users.noreply.github.com>
2023-09-22 21:19:55 -0700

    Delete homework7 directory

--- homework7/print_digits.py
+++
@@ -1,5 +0,0 @@
-def print_digits(n):
-    q = n % 10
-    print (q)
-    if n // 10 > 0:
-        print_digits(n // 10)
--- homework7/quotient.py
+++
@@ -1,4 +0,0 @@
-def quotient(n,d):
-    if n < d:
-        return 0
-    return 1 + quotient(n-d, d)
